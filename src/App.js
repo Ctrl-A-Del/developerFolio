27ea743ad10ca162c273040d0e61a552ac95dcb2
Ctrl-A-Del <github@mailwarden.de>
2021-05-02 22:31:32 +0200

    add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LegalNotice from "./components/legalNotice/LegalNotice";
 import Main from "./containers/Main";
 import React from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import ScrollToTop from "./components/scrollToTop/ScrollToTop";
 
 function Portfolio() {
   return (
@@ -15,10 +16,11 @@ function Portfolio() {
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/" component={Portfolio} />
-        <Route exact path="/legal-notice" component={LegalNotice} />
-      </Switch>
+      <ScrollToTop />
+        <Switch>
+          <Route exact path="/" component={Portfolio} />
+          <Route path="/legal-notice" component={LegalNotice} />
+        </Switch>
     </Router>
   );
 }
