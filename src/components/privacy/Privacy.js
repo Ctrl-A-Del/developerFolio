d3587a35eab8f902a1de8fb7c4f5c7ed6af02043
Ctrl-A-Del <gitlab@mailwarden.de>
2023-06-15 17:38:22 +0200

    fix all components getting deleted and adjust footer

---
+++ src/components/privacy/Privacy.js
@@ -0,0 +1,45 @@
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
+import "./Privacy.css";
+import PrivacyText from "./PrivacyText";
+import React, {Component} from "react";
+import {StyleProvider} from "../../contexts/StyleContext";
+import Top from "../../containers/topbutton/Top";
+
+export default class Privacy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDark: false
+    };
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    if (localStorage.getItem("isDark") === null) {
+      const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
+      localStorage.setItem("isDark", darkPref.matches);
+    }
+    this.setState({isDark: JSON.parse(localStorage.getItem("isDark"))});
+  }
+  changeTheme = () => {
+    this.setState({isDark: !this.state.isDark}, () => {
+      localStorage.setItem("isDark", this.state.isDark);
+    });
+  };
+
+  render() {
+    return (
+      <div className={this.state.isDark ? "dark-mode" : null}>
+        <StyleProvider
+          value={{isDark: this.state.isDark, changeTheme: this.changeTheme}}
+        >
+          <Header />
+          <PrivacyText />
+          <Footer />
+          <Top />
+        </StyleProvider>
+      </div>
+    );
+  }
+}
